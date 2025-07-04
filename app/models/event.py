6d12dfb9a6f92fe98f037d425eabb1e16bd49001
Samuele Tolomeo <samuele@tolomeo.dev>
2025-07-04 18:18:26 +0200

    implementato il paradigma visto a lezione

--- app/models/event.py
+++ app/models/event.py
@@ -1,5 +1,18 @@
+from datetime import datetime
 from sqlmodel import SQLModel, Field
 
 
-class Event(SQLModel, table=True):
-    id: int = Field(primary_key=True)
+class EventBase(SQLModel):
+    description: str
+    location: str
+    date: datetime
+
+class EventCreate(EventBase):
+    pass
+
+class EventPublic(EventBase):
+    id: int
+
+class Event(EventBase, table=True):
+    id: int = Field(default=None, primary_key=True)
+
--- app/models/user.py
+++ app/models/user.py
@@ -1,5 +1,14 @@
 from sqlmodel import SQLModel, Field
 
 
-class User(SQLModel, table=True):
-    username: str = Field(primary_key=True)
+class UserBase(SQLModel):
+    username: str
+
+class UserCreate(UserBase):
+    pass
+
+class UserPublic(UserBase):
+    id: int
+
+class User(UserBase, table=True):
+    id: int = Field(default=None, primary_key=True)
